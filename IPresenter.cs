38a75e5c8e74d30c50970e2785aa163e01cf6fe1
Tri Nguyen <tringuyen.nnt@gmail.com>
2018-07-01 14:46:56 +0700

    init source code for Core project

---
+++ IPresenter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleMVP
+{
+    public interface IPresenter
+    {
+        /// <summary>
+        /// Attach the view to Presenter
+        /// Called by platform when the View is loaded and attached to the Window
+        /// iOS: ViewWillAppear()
+        /// Android: Called on OnCreate() by Fragment, OnViewCreated() on Activity
+        /// </summary>
+        /// <param name="view">View.</param>
+        void AttachView(IView view);
+
+        /// <summary>
+        /// Notify that the view is resuming, register subscription and start job
+        /// Android: OnStart()
+        /// iOS: ViewWillAppear()
+        /// </summary>
+        void OnAppearing();
+
+        /// <summary>
+        /// Notify that the view is pausing/disappearing, unregister all subscription and stop job
+        /// Android: OnStop()
+        /// iOS: ViewDidDisappear()
+        /// </summary>
+        void OnDisappearing();
+
+        /// <summary>
+        /// Detach View from Window
+        /// Android: OnStop()
+        /// iOS: ViewDidDisappear()
+        /// </summary>
+        void DetachView();
+    }
+}
---
+++ PresenterBase.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace SimpleMVP
+{
+    public abstract class PresenterBase : IPresenter
+    {
+        private WeakReference<IView> _weakRefView;
+
+        protected IView View
+        {
+            get
+            {
+                if (_weakRefView != null && _weakRefView.TryGetTarget(out var view))
+                {
+                    return view;
+                }
+
+                return default(IView);
+            }
+            private set
+            {
+                if (value == null)
+                {
+                    _weakRefView?.SetTarget(null);
+                    _weakRefView = null;
+                }
+                else
+                {
+                    if (_weakRefView == null)
+                    {
+                        _weakRefView = new WeakReference<IView>(value);
+                    }
+                    else
+                    {
+                        _weakRefView.SetTarget(value);
+                    }
+                }
+            }
+        }
+
+        public void AttachView(IView view)
+        {
+            View = view;
+            OnViewAttached();
+        }
+        
+        public void DetachView()
+        {
+            View = default(IView);
+            OnViewDetached();
+        }
+
+        /// <summary>
+        /// View is loaded and attached to the Window and ready to start working
+        /// Should init data from here
+        /// </summary>
+        protected virtual void OnViewAttached()
+        {
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// View is about to be shown, start load/populate the data
+        /// </summary>
+        public abstract void OnAppearing();
+
+        /// <inheritdoc />
+        /// <summary>
+        /// View is about to be hidden, should prepare to pause work
+        /// There is no need to stop all the pending works because of this methods, the View is not cleared but only hidden
+        /// </summary>
+        public abstract void OnDisappearing();
+
+        /// <summary>
+        /// View is detached from the Window and stop working
+        /// </summary>
+        protected virtual void OnViewDetached()
+        {
+        }
+    }
+
+    public abstract class PresenterBase<TView> : PresenterBase where TView : IView
+    {
+        protected new TView View
+        {
+            get
+            {
+                IView view = base.View;
+                if (view is TView typedView)
+                    return typedView;
+
+                return default(TView);
+            }
+        }
+    }
+}
